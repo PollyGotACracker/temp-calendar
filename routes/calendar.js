cb7d0c165bed6dd21ce0263bfb5ce0351ad015f8
callor <callor@callor.com>
2022-11-22 15:42:13 +0900

    데이터리스트

--- routes/calendar.js
+++ routes/calendar.js
@@ -8,13 +8,24 @@ const router = express.Router();
 router.get("/", async (req, res, next) => {
   const holidays = await Holiday.findAll();
   const holiArr = [];
+  const areaInfo = [
+    { id: "seoul", name: "서울", eng: "seoul" },
+    { id: "gwangju", name: "광주", eng: "gwangju" },
+  ];
+
+  const genres = [
+    { g_id: "jazz", g_name: "째즈" },
+    { g_id: "trot", g_name: "트롯" },
+    { g_id: "classic", g_name: "클래식" },
+  ];
+
   // for (let i of holidays) {
   //   holiArr.push(i.dataValues);
   // }
   // console.log(holidays);
   // res.json(holidays);
   //
-  res.render("calendar", { holidays: Promise.all(holidays) });
+  res.render("calendar", { holidays, areaInfo, genres });
 });
 
 export default router;
